7b38b658240765b2a47925cbd458ba91a78b040c
Carlos Cardona <carlos@zazengo.com>
2010-10-10 20:17:19 -0700

    added semi colons to everything for jslint. also concatenating the 4 drop shadow input fields into the proper css value

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,29 @@
  $(document).ready(function() {
     $("#submit-button").click(function() {
       // get input value and assign to var
-      var newBorderRadiusValue = $("#newBorderRadiusInput").val()
+      var newBorderRadiusValue = $("#newBorderRadiusInput").val();
 
       // get input value and assign to var
-      var newBoxShadowValue = $("#newBoxShadowInput").val()
+      var newBoxShadowColorValue = $("#newBoxShadowColorInput").val();
+      var newBoxShadowXValue = $("#newBoxShadowXInput").val();
+      var newBoxShadowYValue = $("#newBoxShadowYInput").val();
+      var newBoxShadowFadeValue = $("#newBoxShadowFadeInput").val();
+      var newBoxShadowValue = newBoxShadowColorValue + " " + newBoxShadowXValue + " " + newBoxShadowYValue + " " + newBoxShadowFadeValue;
 
       // get input value and assign to var
-      var newTextShadowValue = $("#newTextShadowInput").val()
+      var newTextShadowValue = $("#newTextShadowInput").val();
 
       // change the border-radius value of #dynamic
-      $("#dynamic").css("border-radius", newBorderRadiusValue + "px")
+      $("#dynamic").css("border-radius", newBorderRadiusValue + "px");
 
       // change the -webkit-box-shadow value of #dynamic
-      $("#dynamic").css("webkit-box-shadow", newBoxShadowValue)
+      $("#dynamic").css("webkit-box-shadow", newBoxShadowValue);
 
       // change the text-shadow value of #dynamic
-      $("#dynamic").css("text-shadow", newTextShadowValue)
+      $("#dynamic").css("text-shadow", newTextShadowValue);
 
       // call refreshFooter()
-      refreshFooter()
-    }) // end of click
-    refreshFooter()
-  })
+      refreshFooter();
+    }); // end of click
+    refreshFooter();
+  });
